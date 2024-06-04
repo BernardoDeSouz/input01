0ffe2b11210b62c8226d31b8cca9aee2e332b160
Aluno <Aluno@10.85.198.40>
2024-06-04 16:40:05 -0300

    Commit project input01

---
+++ src/Input01.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+public class Input01 {
+    public static void main(String[] args) {
+        //Create a JOptionPane.
+       String input = JOptionPane.showInputDialog("Teste, escreve ai");
+        //Store the input as a String and print it.
+        //Parse the input as an int.
+        //Print its value +1
+        //Try creating a dialog, parsing it, and initializing an int in a single line.
+        //You should have only one semicolon (;) in this line.
+
+        
+    }
+}
